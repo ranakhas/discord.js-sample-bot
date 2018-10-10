2bc2eb03c7cb32bc63e07cb5334fde1e3f1c1e3f
Yağızhan Burak Yakar <35808275+yagizhan49@users.noreply.github.com>
2018-10-10 20:29:55 +0300

    Crash fix
    Added a fix that will avoid client crash on a failure.
    Now it will just log it on console instead and stop crashing because it now handles errors.

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ client.on("ready", () => {
   client.user.setActivity(`Serving ${client.guilds.size} servers`);
 });
 
+client.on('error', console.error);
+
 client.on("guildCreate", guild => {
   // This event triggers when the bot joins a guild.
   console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
