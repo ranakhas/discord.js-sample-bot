bc0185d6adf203341d611bfbb1ce57462cbe5dd3
ranakhas <33785157+ranakhas@users.noreply.github.com>
2018-11-30 20:45:11 +0500

    Update index.js

--- index.js
+++ index.js
@@ -130,6 +130,36 @@ client.on("message", async message => {
     message.channel.bulkDelete(fetched)
       .catch(error => message.reply(`I'm not able to delete messages because: ${error}`));
   }
+  let question = args.slice(0).join(' ');
+    if (!question)
+    return message.reply("Please ask a question!");
+    var sayings = [ ':8ball: Absolutly.',
+                    ':8ball: Absolutly not.',
+                    ':8ball: It is true.',
+                    ':8ball: Impossible.',
+                    ':8ball: Of course.',
+                    ':8ball: I do not think so.',
+                    ':8ball: It is true.',
+                    ':8ball: It is not true.',
+                    ':8ball: I am very undoubtful of that.',
+                    ':8ball: I am very doubtful of that.',
+                    ':8ball: Sources point to no.',
+                    ':8ball: Theories prove it.',
+                    ':8ball: Reply hazy try again.',
+                    ':8ball: Ask again later.',
+                    ':8ball: Better not tell you now.',
+                    ':8ball: Cannot predict now.',
+                    ':8ball: Concentrate and ask again.'
+        ];
+
+        var result = Math.floor((Math.random() * sayings.length) + 0);
+        message.channel.send(sayings[result]);
+share improve this question
+asked
+Feb 28 at 16:48
+
+Erkaloth
+
 });
 
 client.login(config.token);
